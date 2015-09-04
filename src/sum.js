7ecf1b32a8d806336bb16253f30897e2abdd7938
Bruno Scopelliti <bru.scopelliti@gmail.com>
2015-09-04 07:54:59 +0200

    Array.from vs Array#slice
    In order to make the build pass this time, i fix the sum function,
    using the old fashioned way of transforming array-like object into real
    array.

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,8 @@
 
 function sum(){
-    var args = Array.from(arguments);
+    // var args = Array.from(arguments);
+    // use the previous expression if you want to see the build fail on IE
+    var args = Array.prototype.slice.apply(arguments);
     return args.length == 0 ? 0 : args.reduce(function(a,b) { return a+b; });
 }
 
